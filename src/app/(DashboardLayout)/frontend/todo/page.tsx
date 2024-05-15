641834309ce44904620926d1bf2f72997a6ba7d0
Fiuse <114389537+Fiw0202@users.noreply.github.com>
2024-05-15 23:41:59 +0700

    complete auto Todo list
    - UI
    - Function

--- src/app/(DashboardLayout)/frontend/todo/page.tsx
+++ src/app/(DashboardLayout)/frontend/todo/page.tsx
@@ -1,21 +1,80 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import BaseCard from "../../components/shared/BaseCard";
-import { Typography, Grid, Box, Button, } from "@mui/material";
+import { Typography, Grid, Box } from "@mui/material";
 import { mockupItems } from "./mockupItems";
 
+interface ISelectedItem {
+  type: string;
+  name: string;
+  group?: number;
+}
+
+interface IParamsFilter {
+  group?: number;
+  type?: string;
+}
 
 const TodoList = () => {
+  const newList = mockupItems.map((m) => ({ ...m, group: 1 }));
+
+  const [listPlantData, setPlantData] = useState<ISelectedItem[]>(newList);
+  const [selectedItems, setSelectedItems] = useState<ISelectedItem[]>([]);
+
+  const filteredData = ({ group, type }: IParamsFilter) => {
+    const data = listPlantData?.filter(
+      (f, index, arr) =>
+        f.group === group &&
+        (type === undefined || f?.type === type) &&
+        index ===
+          arr.findIndex(
+            (fi) =>
+              fi.type === f.type && fi.name === f.name && fi.group === f.group
+          )
+    );
+    return data;
+  };
+
+  const handleSelectItem = (item: ISelectedItem) => {
+    if (item.type === "Fruit") {
+      item.group = 2;
+    } else {
+      item.group = 3;
+    }
+    setPlantData((prevItems) => [...prevItems, item]);
+    setSelectedItems((prevItems) => [...prevItems, item]);
+  };
+
+  const handleDeselect = (item: ISelectedItem) => {
+    item.group = 1;
+    setPlantData((prevItems) => [...prevItems, item]);
+  };
+
+  const autoDeSelect = () => {
+    if (selectedItems.length > 0) {
+      const updatedSelectedItems = selectedItems.slice(1);
+      const autoList = selectedItems[0];
+      autoList.group = 1;
+      setPlantData((prevItems) => [...prevItems, autoList]);
+      setSelectedItems(updatedSelectedItems);
+    }
+  };
+
+  if (selectedItems.length > 0) {
+    setTimeout(async () => autoDeSelect(), 5000);
+  }
+
   return (
     <BaseCard title="Auto Delete Todo List">
-      <Grid container spacing={2} direction="row" justifyContent="center">
-        <Grid item xs={4} md={4} sx={{ border: "1px solid red" }}>
-          <Typography variant="h4" align="left" mb={2}>
+      <Grid container spacing={2} direction="row">
+        {/*Items For Select*/}
+        <Grid item xs={4} md={4} sx={{ border: "1px solid #e7e7e7" }}>
+          <Typography variant="h4" align="left" mb={3} p={2}>
             List
           </Typography>
           <Grid container spacing={2} justifyContent="center" mb={2}>
-            {mockupItems?.map((m, index) => {
+            {filteredData({ group: 1 })?.map((m, index) => {
               return (
                 <Grid item xs={10} p={0} key={index}>
                   <Box
@@ -32,7 +91,7 @@ const TodoList = () => {
                         backgroundColor: "#e7e7e7",
                       },
                     }}
-                    onClick={() => alert(123)}
+                    onClick={(e) => handleSelectItem(m)}
                   >
                     <Typography variant="h4" align="center" key={index}>
                       {m?.name}
@@ -44,15 +103,90 @@ const TodoList = () => {
           </Grid>
         </Grid>
 
-        <Grid item xs={4} md={4} sx={{ border: "1px solid red" }}>
-          <Typography variant="h4" align="center">
-            Todo
+        {/*Fruit Type*/}
+        <Grid item xs={4} md={4} sx={{ border: "1px solid #e7e7e7" }}>
+          <Typography
+            variant="h4"
+            align="left"
+            mb={3}
+            mr={2}
+            padding={2}
+            borderRadius={2}
+            sx={{ backgroundColor: "#e7e7e7" }}
+          >
+            Fruit
           </Typography>
+          <Grid container spacing={2} justifyContent="center" mb={2}>
+            {filteredData({ group: 2, type: "Fruit" })?.map((m, index) => {
+              return (
+                <Grid item xs={10} p={0} key={index}>
+                  <Box
+                    height={60}
+                    justifyItems="center"
+                    gap={4}
+                    p={2}
+                    borderRadius={1}
+                    sx={{
+                      border: "1px solid #e1e1e1",
+                      boxShadow: " 0px 10px 15px -3px rgba(0,0,0,0.1)",
+                      "&:hover": {
+                        cursor: "pointer",
+                        backgroundColor: "#e7e7e7",
+                      },
+                    }}
+                    onClick={(e) => handleDeselect(m)}
+                  >
+                    <Typography variant="h4" align="center" key={index}>
+                      {m?.name}
+                    </Typography>
+                  </Box>
+                </Grid>
+              );
+            })}
+          </Grid>
         </Grid>
-        <Grid item xs={4} md={4} sx={{ border: "1px solid red" }}>
-          <Typography variant="h4" align="center">
-            Todo
+
+        {/*Vegetable Type*/}
+        <Grid item xs={4} md={4} sx={{ border: "1px solid #e7e7e7" }}>
+          <Typography
+            variant="h4"
+            align="left"
+            mb={3}
+            mr={2}
+            padding={2}
+            borderRadius={2}
+            sx={{ backgroundColor: "#e7e7e7" }}
+          >
+            Vegetable
           </Typography>
+          <Grid container spacing={2} justifyContent="center" mb={2}>
+            {filteredData({ group: 3, type: "Vegetable" })?.map((m, index) => {
+              return (
+                <Grid item xs={10} p={0} key={index}>
+                  <Box
+                    height={60}
+                    justifyItems="center"
+                    gap={4}
+                    p={2}
+                    borderRadius={1}
+                    sx={{
+                      border: "1px solid #e1e1e1",
+                      boxShadow: " 0px 10px 15px -3px rgba(0,0,0,0.1)",
+                      "&:hover": {
+                        cursor: "pointer",
+                        backgroundColor: "#e7e7e7",
+                      },
+                    }}
+                    onClick={(e) => handleDeselect(m)}
+                  >
+                    <Typography variant="h4" align="center" key={index}>
+                      {m?.name}
+                    </Typography>
+                  </Box>
+                </Grid>
+              );
+            })}
+          </Grid>
         </Grid>
       </Grid>
     </BaseCard>
@@ -60,6 +194,3 @@ const TodoList = () => {
 };
 
 export default TodoList;
-function styled(Paper: any) {
-  throw new Error("Function not implemented.");
-}
