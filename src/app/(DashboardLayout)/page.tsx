362febad5753fcbb90a1adfcdeae5d45e22db9c6
Fiuse <114389537+Fiw0202@users.noreply.github.com>
2024-05-15 23:40:54 +0700

    Update page.tsx
    รับ Props children

--- src/app/(DashboardLayout)/page.tsx
+++ src/app/(DashboardLayout)/page.tsx
@@ -1,13 +1,13 @@
-"use client";
+// "use client";
 import { Box } from "@mui/material";
 import PageContainer from "@/app/(DashboardLayout)/components/container/PageContainer";
 
 // components
 
-const Dashboard = () => {
+const Dashboard = ({ children }: { children: React.ReactNode }) => {
   return (
     <PageContainer title="7Solution Assignments">
-      <Box mt={3}></Box>
+      <Box mt={3}>{children}</Box>
     </PageContainer>
   );
 };
