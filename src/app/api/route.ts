612c7fc5b5c5cf014968ea9002040f4a700fc227
Fiuse <114389537+Fiw0202@users.noreply.github.com>
2024-05-21 00:43:26 +0700

    ย้าย interface ไปที่ folder
    - ย้าย interface ไปที่ folder

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,4 @@
+import { IResp } from "./interfaces";
 
 export async function GET() {
   try {
@@ -8,90 +9,4 @@ export async function GET() {
     console.log(error);
     throw error;
   }
-}
-
-interface ICryptoDetail {
-  coin?: string;
-  wallet?: string;
-  network?: string;
-}
-
-interface ICompanyDetailAdrCoor {
-  lat?: string;
-  lng?: string;
-}
-
-interface ICompanyDetailAdr {
-  address?: string;
-  city?: string;
-  coordinates?: ICompanyDetailAdrCoor;
-  postalCode?: string;
-  state?: string;
-}
-
-interface IUserCompany {
-  address?: ICompanyDetailAdr;
-  department?: string;
-  name?: string;
-  title?: string;
-}
-
-interface IBankDetail {
-  cardExpire?: string;
-  cardNumber?: string;
-  cardType?: string;
-  currency?: string;
-  iban?: string;
-}
-
-interface IAddressCoor {
-  lat?: string;
-  lng?: string;
-}
-
-interface IUserAddressDetail {
-  address?: string;
-  city?: string;
-  coordinates?: IAddressCoor;
-  postalCode?: string;
-  state?: string;
-}
-
-interface IUserHairStyle {
-  color?: string;
-  type?: string;
-}
-export interface IUserDetail {
-  id?: number;
-  firstName?: string;
-  lastName?: string;
-  maidenName?: string;
-  age?: number;
-  gender?: string;
-  email?: string;
-  phone?: string;
-  username?: string;
-  password?: string;
-  birthDate?: string;
-  image?: string;
-  bloodGroup?: string;
-  height?: number;
-  weight?: number;
-  eyeColor?: string;
-  hair?: IUserHairStyle;
-  domain?: string;
-  ip?: string;
-  address?: IUserAddressDetail;
-  macAddress?: string;
-  university?: string;
-  bank?: IBankDetail;
-  company: IUserCompany;
-  ein?: string;
-  ssn?: string;
-  userAgent?: string;
-  crypto?: ICryptoDetail;
-}
-
-export interface IResp {
-  users: IUserDetail[];
 }
